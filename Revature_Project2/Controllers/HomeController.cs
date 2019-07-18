07fbd6ba06c662e74829c121a0a205ec46f90043
Amzad <me@amzad.dev>
2019-07-18 12:59:43 -0400

    Added API Program

--- Revature_Project2/Controllers/HomeController.cs
+++ Revature_Project2/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -14,9 +15,16 @@ namespace Revature_Project2.Controllers
     public class HomeController : Controller
     {
         ApplicationDbContext _db;
-        public HomeController(ApplicationDbContext db)
+        private readonly IHttpClientFactory _clientFactory;
+        public IEnumerable<GitHubBranch> Branches { get; private set; }
+        /*public HomeController(ApplicationDbContext db)
         {
             _db = db;
+        }*/
+
+        public HomeController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
         }
         public IActionResult Index()
         {
@@ -25,17 +33,42 @@ namespace Revature_Project2.Controllers
         }
 
         [Authorize]
-        public IActionResult Privacy()
+        public async Task<IActionResult> Privacy()
         {
-            _db.Orders.Add(
-                new Order()
-                {
-                    CustomerID = this.User.FindFirstValue(ClaimTypes.NameIdentifier),
-                    OrderDateTime = DateTime.Now,
-                    OrderPrice = 10
-                }); ;
+            /* _db.Orders.Add(
+                 new Order()
+                 {
+                     CustomerID = this.User.FindFirstValue(ClaimTypes.NameIdentifier),
+                     OrderDateTime = DateTime.Now,
+                     OrderPrice = 10
+                 }); 
+             _db.SaveChanges();*/
 
-            _db.SaveChanges();
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                "https://api.github.com/repos/Amzad/Revature_Project2/branches");
+            request.Headers.Add("Accept", "application/vnd.github.v3+json");
+            request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Branches = await response.Content
+                    .ReadAsAsync<IEnumerable<GitHubBranch>>();
+                foreach (var b in Branches)
+                {
+                    System.Diagnostics.Debug.WriteLine(b.Name);
+                }
+            }
+            else
+            {
+                //GetBranchesError = true;
+                Branches = Array.Empty<GitHubBranch>();
+            }
+
+
             return View();
 
         }
